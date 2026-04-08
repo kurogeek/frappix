974d136f6d190a937e55f1e1a5332bdf96f57fab
kurogeek <kurogeek@lmvhaus.com>
2026-04-08 11:27:31 +0700

    bump frappe

--- apps/sources/frappe.nix
+++ apps/sources/frappe.nix
@@ -1,15 +1,15 @@
 {
   pname = "frappe";
-  version = "v15.60.0";
+  version = "v15.104.0";
   meta = {
-    url = "https://github.com/frappe/frappe/releases/tag/v15.60.0";
-    description = "Sources for frappe (v15.60.0)";
+    url = "https://github.com/frappe/frappe/releases/tag/v15.104.0";
+    description = "Sources for frappe (v15.104.0)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe"; repo = "frappe";
-    narHash = "sha256-JoZfT0ZAVV76voyANlLOn15FgjdGMLT1L+30KZmvVQQ=";
-    rev = "b38a313be1d63e41160115af3e359af0fd977ed8";
+    narHash = "sha256-h/B2EBh90Sy+6wZQGnrAAriLujESRiz6AMsN/7bRkhk=";
+    rev = "79f10f8769a9a839abaef1f220da2d2eea5eea27";
   };
   passthru = builtins.fromJSON ''{"clone": {"since": "version-14", "upstream": {"fetch": ["+refs/heads/develop:refs/remotes/upstream/develop", "+refs/heads/version-15:refs/remotes/upstream/version-15", "+refs/heads/version-15-hotfix:refs/remotes/upstream/version-15-hotfix", "+refs/tags/v15.*:refs/remotes/upstream/tags/v15.*"], "url": "https://github.com/frappe/frappe"}}}'';
 }
