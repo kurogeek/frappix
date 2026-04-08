b159a28bca4a5110bc841f29f40540a3aadee3fc
kurogeek <kurogeek@lmvhaus.com>
2026-04-08 11:27:20 +0700

    bump erpnext

--- apps/sources/erpnext.nix
+++ apps/sources/erpnext.nix
@@ -1,15 +1,15 @@
 {
   pname = "erpnext";
-  version = "v15.54.5";
+  version = "v15.104.0";
   meta = {
-    url = "https://github.com/frappe/erpnext/releases/tag/v15.54.5";
-    description = "Sources for erpnext (v15.54.5)";
+    url = "https://github.com/frappe/erpnext/releases/tag/v15.104.0";
+    description = "Sources for erpnext (v15.105.0)";
   };
   src = builtins.fetchTree {
     type = "github";
     owner = "frappe"; repo = "erpnext";
-    narHash = "sha256-gKO3WDbaS+8iGX+T4N0KNE8hGT34cGRK3LhVPkQ8FBw=";
-    rev = "35ac96f1ec09de17916c447e172e8464f3c160be";
+    narHash = "sha256-JYuyeoV7bbMj+oS9cyXsmBOuwHsXGqaHrHTN9l+1u3E=";
+    rev = "a2626ed55f437f69b99a29cfb0b9ead219f59458";
   };
   passthru = builtins.fromJSON ''{"since": "version-14", "upstream": "URL: https://github.com/frappe/erpnext\nPull: +refs/heads/develop:refs/remotes/upstream/develop\nPull: +refs/heads/version-15:refs/remotes/upstream/version-15\nPull: +refs/heads/version-15-hotfix:refs/remotes/upstream/version-15-hotfix\nPull: +refs/tags/v15.*:refs/remotes/upstream/tags/v15.*\n"}'';
 }
