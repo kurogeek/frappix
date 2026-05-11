5cc339a3968e87809b793b5cd94b20e1a663d6f2
kurogeek <kurogeek@lmvhaus.com>
2026-05-11 17:01:50 +0700

    crm built

--- apps/sources/crm.nix
+++ apps/sources/crm.nix
@@ -1,15 +1,15 @@
 {
   pname = "crm";
-  version = "v1.39.1";
+  version = "v1.71.0";
   meta = {
-    url = "https://github.com/frappe/crm/releases/tag/v1.39.1";
-    description = "Sources for crm (v1.39.1)";
+    url = "https://github.com/frappe/crm/releases/tag/v1.71.0";
+    description = "Sources for crm (v1.71.0)";
   };
   src = builtins.fetchTree {
     type = "git";
     url = "https://github.com/frappe/crm.git"; submodules = true; allRefs = true;
-    narHash = "sha256-ILiVSDT6SLmxhPFkPIf8qL5cZD1aAB1xuKy2zioOYxg=";
-    rev = "bf4da21153d2f1caab45f3e11f95cbea119368e5";
+    narHash = "sha256-3nWYvFqoGzArxHn0ydZYRgXDbquLH0Pk+t6kUKpikLQ=";
+    rev = "20e87b3f899bfcedaaf326bef7869cdd890bb956";
   };
   passthru = builtins.fromJSON ''{}'';
 }
--- src/overlays/frappe/crm.nix
+++ src/overlays/frappe/crm.nix
@@ -4,13 +4,106 @@
   buildPythonPackage,
   pythonRelaxDepsHook,
   flit-core,
-  # rembg,
   python,
   extractFrappeMeta,
-  mkAssets,
   applyPatches,
+  stdenv,
+  fetchYarnDeps,
+  nodejs,
+  yarnConfigHook,
 }:
-buildPythonPackage rec {
+let
+  src = applyPatches{
+    inherit (appSources.crm) src;
+    name = "patched-crm";
+    patches = [
+      ./crm-0001-build-socket-port-is-reverse-proxied.patch
+    ];
+  };
+  version = appSources.hrms.version;
+
+
+  frontend = stdenv.mkDerivation (finalAttrs: {
+    pname = "crm-frontend";
+    inherit version;
+
+    src = "${src}/frontend";
+
+    yarnOfflineCache = fetchYarnDeps {
+      yarnLock = "${finalAttrs.src}/yarn.lock";
+      hash = "sha256-OdETUUCFI1jxGqT4XQiSXVR4FhViSUX1WJjJnSZOcj8=";
+    };
+
+    nativeBuildInputs = [
+      nodejs
+      yarnConfigHook
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+
+      substituteInPlace $TMP/frontend/vite.config.js --replace-fail "../crm/www" out/public/www
+
+      substituteInPlace $TMP/frontend/package.json --replace-fail "../crm/public/frontend" dist
+      substituteInPlace $TMP/frontend/package.json --replace-fail "../crm/www" out/public/www
+
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p out/public/frontend
+      mkdir -p out/public/www
+      mkdir $out
+
+      npm run build
+
+      cp -R dist/* out/public/frontend
+
+      cp -R out/* $out
+
+      runHook postInstall
+    '';
+  });
+
+  crm = stdenv.mkDerivation (finalAttrs: {
+    pname = "crm";
+    inherit version;
+
+    inherit src;
+
+    yarnOfflineCache = fetchYarnDeps {
+      yarnLock = "${finalAttrs.src}/yarn.lock";
+      hash = "sha256-oZgyP0hTU9bxszOVg3Bmiu6yos2d2Inc1Do8To4z8GQ=";
+    };
+
+    nativeBuildInputs = [
+      nodejs
+      yarnConfigHook
+    ];
+
+    dontBuild = true;
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/crm/www
+
+      cp -R crm/public $out/crm
+      cp -R crm/www $out/crm
+      cp -R node_modules $out/crm/public
+
+      cp -R ${frontend}/public/frontend $out/crm/public
+
+      cp -R ${frontend}/public/www/* $out/crm/www
+
+      runHook postInstall
+    '';
+  });
+
+in
+buildPythonPackage {
   inherit
     (extractFrappeMeta src)
     pname
@@ -18,16 +111,18 @@ buildPythonPackage rec {
     format
     ;
 
-  src = mkAssets (appSources.crm
-    // {
-      src = applyPatches {
-        inherit (appSources.crm) src;
-        name = "crm-prod";
-        patches = [
-          ./crm-0001-build-socket-port-is-reverse-proxied.patch
-        ];
-      };
-    });
+  src = stdenv.mkDerivation {
+    pname = "crm_";
+    inherit src version;
+    installPhase = ''
+      mkdir $out
+      cp -R . $out
+      cp -R ${crm}/crm/public/node_modules $out
+
+      cp -R ${crm}/crm/public/frontend $out/crm/public
+      cp -R ${crm}/crm/www/crm.html $out/crm/www/
+    '';
+  };
   inherit (appSources.crm) passthru;
 
   nativeBuildInputs = [
